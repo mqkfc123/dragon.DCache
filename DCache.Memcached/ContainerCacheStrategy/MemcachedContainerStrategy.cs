81c2d62ec8138ba5c898618a270d8e91e480906d
liuyl <mqkfc123@163.com>
2018-06-25 09:27:50 +0800

    mamcached 缓存的支持

---
+++ DCache.Memcached/ContainerCacheStrategy/MemcachedContainerStrategy.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DCache.Memcached.ObjectCacheStrategy;
+using DCache.Cache.ContainerCacheStrategy;
+using DCache.Containers;
+#if NET45 || NET461
+
+#else
+ 
+#endif
+
+
+namespace DCache.Memcached.ContainerCacheStrategy
+{
+    public class MemcachedContainerStrategy : MemcachedObjectCacheStrategy, IContainerCacheStrategy
+    {
+
+        #region 单例
+        /// <summary>
+        /// LocalCacheStrategy的构造函数
+        /// </summary>
+        MemcachedContainerStrategy()
+        {
+        }
+
+        //静态LocalCacheStrategy
+        public static new IContainerCacheStrategy Instance
+        {
+            get
+            {
+                return Nested.instance;//返回Nested类中的静态成员instance
+            }
+        }
+
+        class Nested
+        {
+            static Nested()
+            {
+            }
+            //将instance设为一个初始化的LocalCacheStrategy新实例
+            internal static readonly MemcachedContainerStrategy instance = new MemcachedContainerStrategy();
+        }
+        #endregion
+
+
+        #region IContainerCacheStrategy 成员
+
+        public void InsertToCache(string key, IBaseContainerBag value)//TODO:添加Timeout参数
+        {
+            if (string.IsNullOrEmpty(key) || value == null)
+            {
+                return;
+            }
+
+            base.InsertToCache(key, value);
+#if DEBUG
+            var cacheKey = GetFinalKey(key);
+            value = _cache.Get(cacheKey) as IBaseContainerBag;
+#endif
+        }
+
+        public override void RemoveFromCache(string key, bool isFullKey = false)
+        {
+            base.RemoveFromCache(key, isFullKey);
+        }
+
+        public new IBaseContainerBag Get(string key, bool isFullKey = false)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return null;
+            }
+
+            var cacheKey = GetFinalKey(key, isFullKey);
+            return _cache.Get<IBaseContainerBag>(cacheKey);
+        }
+
+        public IDictionary<string, TBag> GetAll<TBag>() where TBag : IBaseContainerBag
+        {
+            throw new NotImplementedException();
+        }
+
+        public new IDictionary<string, IBaseContainerBag> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CheckExisted(string key, bool isFullKey = false)
+        {
+            return base.CheckExisted(key, isFullKey);
+        }
+
+        public override long GetCount()
+        {
+            throw new NotImplementedException();//TODO:需要定义二级缓存键，从池中获取
+        }
+
+        public new void Update(string key, IBaseContainerBag value, bool isFullKey = false)
+        {
+            base.Update(key, value, isFullKey);
+        }
+
+        public void UpdateContainerBag(string key, IBaseContainerBag containerBag, bool isFullKey = false)
+        {
+            var cacheKey = GetFinalKey(key, isFullKey);
+            object value;
+            if (_cache.TryGet(cacheKey, out value))
+            {
+                Update(cacheKey, containerBag, true);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ DCache.Memcached/Register.cs
@@ -0,0 +1,40 @@
+﻿using DCache.Cache;
+using DCache.Cache.ObjectCacheStrategy;
+using DCache.Memcached.ObjectCacheStrategy;
+using DCache.RegisterServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCache.Memcached
+{
+    public static class Register
+    {
+
+        /// <summary>
+        /// 注册 Memcached 缓存信息
+        /// </summary>
+        /// <param name="registerService">RegisterService</param>
+        /// <param name="memcachedConfig">memcached连接字符串列表</param>
+        /// <param name="memcachedObjectCacheStrategyInstance">缓存策略的委托，第一个参数为 memcachedConfig</param>
+        /// <returns></returns>
+        public static IRegisterService RegisterCacheMemcached(this IRegisterService registerService,
+            Dictionary<string, int> memcachedConfig,
+            Func<Dictionary<string, int>, IObjectCacheStrategy> memcachedObjectCacheStrategyInstance)
+        {
+            MemcachedObjectCacheStrategy.RegisterServerList(memcachedConfig);
+
+            //此处先执行一次委托，直接在下方注册结果，提高每次调用的执行效率
+            IObjectCacheStrategy objectCacheStrategy = memcachedObjectCacheStrategyInstance(memcachedConfig);
+            if (objectCacheStrategy != null)
+            {
+                CacheStrategyFactory.RegisterObjectCacheStrategy(() => objectCacheStrategy);//Memcached
+            }
+
+            return registerService;
+        }
+
+    }
+}
